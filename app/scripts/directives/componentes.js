0482f685c821d24664c4a0b722d1a1c3c379b689
Jaro Marval <jampgold@gmail.com>
2015-04-04 20:50:47 -0430

    Agregando la directiva de componentes(inputSearch y btnBack)

---
+++ app/scripts/directives/componentes.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name frontendApp.directive:components
+ * @description
+ * # components
+ */
+angular.module('frontendApp')
+.directive('inputSearch', function () {
+    return {
+        restrict: 'E',
+        scope: {
+            model: '=model'
+        },
+        template: "<div class=\"input-search\"><i class=\"fa fa-search\"></i><input type=\"search\" ng-model=\"model\"><i class=\"fa fa-close\" ng-class=\"{hidden: !model}\"></i></div>",
+        link: function(scope, element, attrs) {
+                $(element).find('i.fa-close').on('click', function() {
+                    scope.model = '';
+                    scope.$apply();
+                });
+        }
+    };
+})
+.directive('btnBack', function($window){
+    return {
+        restrict: 'E',
+        template: '<a href class="btn btn-warning"><i class="fa fa-chevron-left"></i> Volver</a>',
+        link: function(scope, element, attrs) {
+            $(element).on('click', function() {
+                $window.history.back();
+            });
+        }
+    };
+});
