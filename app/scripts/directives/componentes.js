4ca6b17f7f9e90a71bfca470423a546bc6433aac
Jaro Marval <jampgold@gmail.com>
2015-04-05 23:07:35 -0430

    Ligera corrección en los componentes

--- app/scripts/directives/componentes.js
+++ app/scripts/directives/componentes.js
@@ -13,8 +13,8 @@ angular.module('frontendApp')
         scope: {
             model: '=model'
         },
-        template: "<div class=\"input-search\"><i class=\"fa fa-search\"></i><input type=\"search\" ng-model=\"model\"><i class=\"fa fa-close\" ng-class=\"{hidden: !model}\"></i></div>",
-        link: function(scope, element, attrs) {
+        template: '<div class="input-search"><i class="fa fa-search"></i><input type="search" ng-model="model"><i class="fa fa-close" ng-class="{hidden: !model}"></i></div>',
+        link: function(scope, element) {
                 $(element).find('i.fa-close').on('click', function() {
                     scope.model = '';
                     scope.$apply();
@@ -26,7 +26,7 @@ angular.module('frontendApp')
     return {
         restrict: 'E',
         template: '<a href class="btn btn-warning"><i class="fa fa-chevron-left"></i> Volver</a>',
-        link: function(scope, element, attrs) {
+        link: function(scope, element) {
             $(element).on('click', function() {
                 $window.history.back();
             });
